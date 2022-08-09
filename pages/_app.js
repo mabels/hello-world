004e7390ea0afb926e927a5530d553174670417d
Meno Abels <meno.abels@adviser.com>
2022-08-09 15:10:44 +0200

    chore: test2

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,42 @@
 import '../styles/globals.css';
+import useSWR from 'swr'
 const uuid  = require('uuid');
 
+const dev = process.env.NODE_ENV !== 'production';
+
+export const server = dev ? 'http://localhost:3000' : 'https://your_deployment.server.com';
+
+export async function getStaticProps () {
+  // `getStaticProps` is executed on the server side.
+  const article = await getArticleFromAPI()
+  return {
+    props: {
+      fallback: {
+        '/api/hello': article
+      }
+    }
+  }
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}>
-    MyApp --- {uuid.v4()}
-    </Component>
+  const my = {...pageProps, uuid: uuid.v4()};
+  return <Component {...my} />;
+}
+
+export const getArticleFromAPI = (url) => {
+  console.log("XXXXXX", url)
+  return fetch(`${server}/api/hello`).then((res) => {
+  console.log(">>>>",res)
+  return res.json()
+})
+}
+
+export function Uuid() {
+  console.log("yyyyyyyy")
+  const bla = useSWR('/api/hello', getArticleFromAPI)
+  console.log("x", bla, "y", bla.data)
+
+  return <div>{bla.data?.name}</div>;
 }
 
 export default MyApp
